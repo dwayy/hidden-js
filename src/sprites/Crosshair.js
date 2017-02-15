b5cdce11bf3242650d70dac45d7a242479c0f16d
dway <>
2017-02-15 11:28:36 +0100

    Adding Crosshair

---
+++ src/sprites/Crosshair.js
@@ -0,0 +1,28 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Sprite {
+
+  constructor ({ game, x, y, asset }) {
+    super(game, x, y, asset)
+    this.anchor.setTo(0.5)
+  }
+
+  move(cursors) {
+    this.body.velocity.x = 0;
+    this.body.velocity.y = 0;
+
+    if (cursors.left.isDown){
+      this.body.velocity.x = -150;
+    }
+    else if (cursors.right.isDown) {
+      this.body.velocity.x = 150;
+    }
+    else if (cursors.up.isDown) {
+      this.body.velocity.y = -150;
+    }
+    else if (cursors.down.isDown) {
+      this.body.velocity.y = 150;
+    }
+  }
+
+}
